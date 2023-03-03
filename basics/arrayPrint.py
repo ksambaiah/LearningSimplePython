5797f84c6bd43ae26c316fd4bae79784a2b158ae
Sambaiah Kilaru <samkilar@cisco.com>
2023-03-03 19:47:04 +0530

    Simple demo how to use git.

---
+++ basics/arrayPrint.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3 
+
+a = [2, 3, 4, 10, 14, 15, 100]
+
+for x in a:
+    print(x)
+
+print("Length of array ", len(a))
+
+for i in range(5):
+    print(i)
+
+for i in range(len(a)):
+    print(a[i])
